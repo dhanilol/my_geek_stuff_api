ada282a93a9d2d0a9714f8830db030ea0896e322
Danilo Santos <danilulp@hotmail.com>
2023-02-22 11:30:07 -0300

    Implementing Pytest
    Adding pytest to dependencies;
    Adding pytest.ini for configuration;
    Including conftest.py as for fixtures;
    Adding basic tests for module_anime;

--- module_anime/tests.py
+++ module_anime/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django import urls
+from django.urls import reverse
+
+from rest_framework import status
+
+
+class TestAnime:
+
+    @pytest.mark.authentication
+    def test_without_authentication(self, api_client):
+        response = api_client.get('/module_anime/anime/')
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.data['detail']
+        assert response.data['detail'] == "Authentication credentials were not provided."
+
+    @pytest.mark.authentication
+    def test_with_wrong_authentication(self, api_client):
+        api_client.credentials(HTTP_AUTHORIZATION='Bearer ' + 'abcd')
+        response = api_client.get('/module_anime/anime/')
+        
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    @pytest.mark.authentication
+    @pytest.mark.django_db
+    def test_with_authentication(self, api_client_with_credentials):
+        response = api_client_with_credentials.get('/module_anime/anime/')
+
+        assert response.status_code == status.HTTP_200_OK
+
+    @pytest.mark.authentication
+    @pytest.mark.django_db
+    def test_with_admin_authentication(self, api_client_with_admin_credentials):
+        response = api_client_with_admin_credentials.get('/module_anime/anime/')
+
+        assert response.status_code == status.HTTP_200_OK
